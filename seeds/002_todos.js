a264d96303682a8f989035ae09f5bbef62e4831d
Mitch Lembke <mitchell.lembke@gmail.com>
2018-05-24 11:11:25 -0600

    populated seeds

--- seeds/002_todos.js
+++ seeds/002_todos.js
@@ -1,13 +1,11 @@
-
-exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
+exports.seed = (knex, Promise) => (
+  knex('todo').del()
+    .then(() => (
+      knex('todo').insert([{
+        id: 1,
+        item: 'Make it work.',
+        user_id: 1
+      }])
+    ))
+    .then(() => knex.raw(`SELECT setval('todo_id_seq', (SELECT MAX(id) FROM todo));`))
+)
